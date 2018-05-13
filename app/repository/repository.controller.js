2bfcce511d603423e70e29316ecad77a4e0a7c8e
Roberto Almanza <almanza.rbto@gmail.com>
2018-05-13 13:10:48 -0400

    Complete the repositories view

--- app/repository/repository.controller.js
+++ app/repository/repository.controller.js
@@ -3,13 +3,34 @@
     .module("Repositories")
     .controller('RepositoryCtrl', repositoryCtrl);
 
-  repositoryCtrl.$inject = ['$routeParams'];
+  repositoryCtrl.$inject = ['$routeParams', 'userResource'];
   
   /**
    * @name repositoryCtrl
    * @desc Github user list controller
+   * @param {Object} $routeParams
+   * @param {Object} userResource
    */
-  function repositoryCtrl($routeParams) {
-    console.log('id:' + $routeParams.userId);
+  function repositoryCtrl($routeParams, userResource) {
+    var vm = this;
+    vm.userName = $routeParams.userId;
+    vm.repositories = [];
+    vm.loadRepositoryByUser = loadRepositoryByUser;
+    loadRepositoryByUser(vm.userName);
+
+    /**
+     * @name loadRepositoryByUser
+     * @desc Retrieve github user repositories
+     * @param {string} userId 
+     */
+    function loadRepositoryByUser(userId) {
+      userResource.getRepositoriesByUser(userId)
+        .then(function (result) {
+          angular.forEach(result.data, function (repo) {vm.repositories.push(repo)});
+        })
+        .catch(function () {
+          window.alert('It is not possible to retrieve "' + $routeParams.userId + '" repositories.');
+        });
+    }
   }
 })();
